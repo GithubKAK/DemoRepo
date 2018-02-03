6e230fc27f9a1d2c1d8ba77a8208281a8d6a15e6
GitHubKAK <Mdkkhan@hotmail.com>
2018-02-03 14:18:16 -0500

    added searchPage

--- src/main/java/com/qa/page/LoginPage.java
+++ src/main/java/com/qa/page/LoginPage.java
@@ -1,5 +1,12 @@
 package com.qa.page;
 
 public class LoginPage {
+	public void loginPage(){
+		System.out.println("Login Page ");
+		
+		}
+	public void logInSet(){
+		System.out.println("Login Set ");
+	}
 
 }
